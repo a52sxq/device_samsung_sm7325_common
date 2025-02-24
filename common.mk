679f35feff799267937f88435975c6674b034144
Andrzej Szafran <rtd1250@gmail.com>
2025-02-24 18:12:18 +0100

    sm7325-common: Build SamsungDAP from source
    Change-Id: I71c06c1df82d5b901b8dcbcdcb8d3dd2556fe3f4

--- common.mk
+++ common.mk
@@ -181,6 +181,10 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.memtrack-service \
     vendor.display.config@2.0.vendor
 
+# SamsungDAP
+PRODUCT_PACKAGES += \
+    SamsungDAP
+
 # DRM
 PRODUCT_PACKAGES += \
     android.hardware.drm-service.clearkey \
