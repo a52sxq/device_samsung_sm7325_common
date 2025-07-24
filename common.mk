af2dce51ca8adf87445332d4cbaed4acda0deb72
Gustavo Mendes <gusttavo.me@outlook.com>
2025-07-23 21:58:46 -0300

    sm7325-common: Import AdvancedDisplay from hw/samsung
    HEAD at:
    - https://github.com/LineageOS/android_hardware_samsung/tree/e79e9239e1f45d3641e0a935a3c4225f3c7f268c/AdvancedDisplay
    
    Change-Id: I978b4e7b90a5431ae31c14830cf616aa24b6333a
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- common.mk
+++ common.mk
@@ -165,8 +165,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@3.0.vendor \
     vendor.qti.hardware.display.mapper@4.0.vendor \
     vendor.qti.hardware.memtrack-service \
-    vendor.display.config@2.0.vendor \
-    AdvancedDisplay
+    vendor.display.config@2.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \
